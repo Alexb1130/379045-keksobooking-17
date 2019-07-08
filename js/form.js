124ded5a98194f537c918dfbde271f1a625f8275
alx <alx@alxs-MacBook-Pro.local>
2019-07-08 23:56:32 +0300

    form validate part 2

--- js/form.js
+++ js/form.js
@@ -10,6 +10,13 @@
     'palace': 10000
   };
 
+  var CapacityProps = {
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '100': '0',
+  };
+
   var disableFields = function (state) {
     var fields = document.querySelectorAll('fieldset');
 
@@ -24,9 +31,20 @@
     var priceField = adForm.elements.price;
     var timeinSelect = adForm.elements.timein;
     var timeoutSelect = adForm.elements.timeout;
+    var roomNumberSelect = adForm.elements.rooms;
+    var capacitySelect = adForm.elements.capacity;
 
     priceField.placeholder = OffersProps[typeSelect.value];
     priceField.min = OffersProps[typeSelect.value];
+
+    if (evt.target === roomNumberSelect) {
+      capacitySelect.value = CapacityProps[roomNumberSelect.value];
+
+      var capacitySelectValues = Array.from(capacitySelect.options).map(function (option) {
+        return parseInt(option.value, 10);
+      });
+
+    }
 
     if (evt.target === timeinSelect || evt.target === timeoutSelect) {
       timeinSelect.value = evt.target.value;
