eb5fd48c22c06dead41ee9bc70df3d6ee05907e6
alx <alx@alxs-MacBook-Pro.local>
2019-07-14 14:42:36 +0300

    filters some refactor

--- js/mapFilters.js
+++ js/mapFilters.js
@@ -6,19 +6,19 @@
   var mapPinsContainer = document.querySelector('.map__pins');
   var data = new window.data.Load('GET', window.data.URL + '/data');
 
-  var onMapFiltered = function (data) {
-    var filteredItems = data.filter(function (item) {
+  var onMapFiltered = function (dataFilters) {
+    var filteredItems = dataFilters.filter(function (item) {
       return housingType.value === item.offer.type;
     });
 
     var filteredMapPins = window.mapPins.generatePins(filteredItems);
 
-    mapPinsContainer.querySelectorAll('.map__pin').forEach(function (pin) {
+    mapPinsContainer.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (pin) {
 
-      if (!pin.classList.contains('map__pin--main')) {
-        pin.remove();
-      } else if (housingType.value === 'any') {
-        window.mapPins.generatePins(data);
+      pin.remove();
+
+      if (housingType.value === 'any') {
+        window.mapPins.generatePins(dataFilters);
       }
     });
 
